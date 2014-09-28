aaa800a82e75ac7c88b07c997322dba63d8560ff
Reo Inoue <inowe.re@gmail.com>
2014-09-28 02:29:21 +0000

    Add integration test to static pages title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,12 @@ describe "Static page" do
 		it "should have the content 'Sample App" do
 	    visit "/static_pages/home"
 	    expect(page).to have_content('Sample App')
-	  end 
+	  end
+
+	  it "should have the title 'Home'" do
+	  	visit '/static_pages/home'
+	  	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
 	end
 
 	describe 'Help page' do
@@ -16,6 +21,11 @@ describe "Static page" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
 		end
+
+    it "should have the title 'Help'" do
+	  	visit '/static_pages/help'
+	  	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
 	end
 
 	describe 'About page' do
@@ -23,6 +33,11 @@ describe "Static page" do
 		it "should have content 'About Us'" do
 			visit '/static_pages/about'
 			expect(page).to have_content("About Us")
+		end
+
+		it "should have the title 'About'" do
+	  	visit '/static_pages/about'
+	  	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
 		end
 	end
 end
