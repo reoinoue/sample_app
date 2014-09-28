3fab7a696c6077fa45d3c4fbe0bfdab780c80fed
Reo Inoue <inowe.re@gmail.com>
2014-09-28 02:17:46 +0000

    Add test to about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,17 +1,28 @@
 require 'spec_helper'
 
-describe "Static pages" do
+describe "Static page" do
+
 	describe "Home page" do
+
 		it "should have the content 'Sample App" do
 	    visit "/static_pages/home"
 	    expect(page).to have_content('Sample App')
 	  end 
 	end
 
-	describe 'Help pages' do
+	describe 'Help page' do
+
 		it "should have content 'Help'" do
 			visit '/static_pages/help'
 			expect(page).to have_content('Help')
+		end
+	end
+
+	describe 'About page' do
+
+		it "should have content 'About Us'" do
+			visit '/static_pages/about'
+			expect(page).to have_content("About Us")
 		end
 	end
 end
