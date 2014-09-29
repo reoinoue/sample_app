738d35b71f15d93ed3091bb94d9267515cc53222
Reo Inoue <inowe.re@gmail.com>
2014-09-29 08:52:45 +0000

    add secure_password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,7 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-  it { should respond_to(:password_degest) }
+  it { should respond_to(:password_digest) }
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
 
