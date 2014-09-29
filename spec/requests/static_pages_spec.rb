dcb2abe540b6c5cbb4d4fc382bed126d48020b51
Reo Inoue <inowe.re@gmail.com>
2014-09-29 02:50:34 +0000

    Refactoring subject

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,56 +3,34 @@ require 'spec_helper'
 describe "Static page" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject {page}
 
   describe "Home page" do
-    before {visit root_path}
+    before { visit root_path }
 
-    it "should have the content 'Sample App" do
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the title 'Home'" do
-      expect(page).to have_title("#{base_title}")
-    end
-
-    it "should not have a custom page title" do
-      expect(page).not_to have_title('| Home')
-    end
+    it { sould have_content('Sample App') }
+    it { sould have_title(full_title('')) }
+    it { sould_not have_title('| Home') }
   end
 
   describe 'Help page' do
-    before {visit help_path}
+    before { visit help_path }
 
-    it "should have content 'Help'" do
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { sould have_content('Help') }
+    it { sould have_title(full_title('Help')) }
   end
 
   describe 'About page' do
-    before {visit about_path}
+    before { visit about_path }
 
-    it "should have content 'About Us'" do
-      expect(page).to have_content("About Us")
-    end
-
-    it "should have the title 'About'" do
-      expect(page).to have_title("#{base_title} | About")
-    end
+    it { sould have_content('About') }
+    it { sould have_title(full_title('About Us')) }
   end
 
   describe 'Contact page' do
-    before {visit contact_path}
+    before { visit contact_path }
 
-    it "should have content 'Contact'" do
-      expect(page).to have_content("Contact")
-    end
-
-    it "should have the title 'Contact'" do
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { sould have_content('Contact') }
+    it { sould have_title(full_title('Contact')) }
   end
 end
