abe3b750eeb75207db7bc37fd568c78b314e8f62
Reo Inoue <inowe.re@gmail.com>
2014-09-29 08:43:25 +0000

    Add test for password and password_confirmations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,13 +2,18 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do
+  	@user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+  end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_degest) }
+  it { should respond_to(:password)}
+  it { should respond_to(:password_confirmation)}
+  
   it { should be_valid }
 
   describe "when name is persent" do
