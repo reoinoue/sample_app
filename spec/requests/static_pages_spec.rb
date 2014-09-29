6b993546547a1a16f4baaa20afc6580ba932d138
Reo Inoue <inowe.re@gmail.com>
2014-09-29 03:06:19 +0000

    Fixed typo

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,29 +7,29 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { sould have_content('Sample App') }
-    it { sould have_title(full_title('')) }
-    it { sould_not have_title('| Home') }
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe 'Help page' do
     before { visit help_path }
 
-    it { sould have_content('Help') }
-    it { sould have_title(full_title('Help')) }
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe 'About page' do
     before { visit about_path }
 
-    it { sould have_content('About') }
-    it { sould have_title(full_title('About Us')) }
+    it { should have_content('About') }
+    it { should have_title(full_title('About Us')) }
   end
 
   describe 'Contact page' do
     before { visit contact_path }
 
-    it { sould have_content('Contact') }
-    it { sould have_title(full_title('Contact')) }
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
   end
 end
