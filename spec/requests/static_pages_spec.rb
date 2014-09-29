a3db578eebcab1d451361740a758393ef8e6a349
Reo Inoue <inowe.re@gmail.com>
2014-09-29 02:22:55 +0000

    Remove redundancy of path

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,58 +5,53 @@ describe "Static page" do
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
 	describe "Home page" do
+    before {visit root_path}
 
 		it "should have the content 'Sample App" do
-	    visit root_path
 	    expect(page).to have_content('Sample App')
 	  end
 
 	  it "should have the title 'Home'" do
-	  	visit root_path
 	  	expect(page).to have_title("#{base_title}")
 		end
 
 		it "should not have a custom page title" do
-			visit root_path
 			expect(page).not_to have_title('| Home')
 		end
 	end
 
 	describe 'Help page' do
+    before {visit help_path}
 
 		it "should have content 'Help'" do
-			visit help_path
 			expect(page).to have_content('Help')
 		end
 
     it "should have the title 'Help'" do
-	  	visit help_path
 	  	expect(page).to have_title("#{base_title} | Help")
 		end
 	end
 
 	describe 'About page' do
+	  before {visit about_path}
 
 		it "should have content 'About Us'" do
-			visit about_path
 			expect(page).to have_content("About Us")
 		end
 
 		it "should have the title 'About'" do
-	  	visit about_path
 	  	expect(page).to have_title("#{base_title} | About")
 		end
 	end
 
 	describe 'Contact page' do
+	  before {visit contact_path}
 
 		it "should have content 'Contact'" do
-			visit contact_path
 			expect(page).to have_content("Contact")
 		end
 
 		it "should have the title 'Contact'" do
-	  	visit contact_path
 	  	expect(page).to have_title("#{base_title} | Contact")
 		end
 	end
