d1f9c3301d38ae4bcb321b775adc0f2ff6cb282f
Reo Inoue <inowe.re@gmail.com>
2014-09-29 08:25:04 +0000

    Add test for presence password_degest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,7 +8,7 @@ describe User do
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
-
+  it { should respond_to(:password_degest) }
   it { should be_valid }
 
   describe "when name is persent" do
