de63bf2761a6d8a3b2d0d795819b8f44dded8510
Reo Inoue <inowe.re@gmail.com>
2014-09-30 18:02:24 +0000

    fix typo

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -30,7 +30,7 @@ describe "Authentication" do
 			let(:user) { FactoryGirl.create(:user) }
 			before do
 				fill_in "Email", with: user.email.upcase
-				fill_in "Password", with: User.password
+				fill_in "Password", with: user.password
 				click_button "Sign in"
 			end
 
