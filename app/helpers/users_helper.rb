9ffe25bb5259d307e647565a6a998983e8e67d16
Reo Inoue <inowe.re@gmail.com>
2014-09-30 08:45:43 +0000

    Add Gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
   # 与えられたユーザのGravater(http://gravater.com/)を返す
-  def gravater_for(user)
-    gravater_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravater_tag(gravatar_url, alt: user.name, class: "gravatar")
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
