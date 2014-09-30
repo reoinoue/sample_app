7209e9c9288617bbe5bd692f4d59337e498bc44f
Reo Inoue <inowe.re@gmail.com>
2014-09-30 18:07:16 +0000

    Add newline

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -17,6 +17,7 @@ describe "Authentication" do
 
 		describe "with invalid information" do
 			before { click_button "Sign in" }
+			
 			it { should have_title('Sign in') }
 			it { should have_selector('div.alert.alert-error', text: 'Invalid')}
 
