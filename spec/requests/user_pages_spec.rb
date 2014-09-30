fd528f398d5303706d91ee6e817bdd0f6bf487e0
Reo Inoue <inowe.re@gmail.com>
2014-09-30 11:06:36 +0000

    fix typo

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,7 +34,7 @@ describe "USER PAGES" do
       before do
         fill_in "Name", with: "Example User"
         fill_in "Email", with: "user@example.com"
-        fill_in "password", with: "foobar"
+        fill_in "Password", with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
 
