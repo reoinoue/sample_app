9c52b2b73997c775283c24c7e497585c591190e7
Reo Inoue <inowe.re@gmail.com>
2014-09-30 08:28:00 +0000

    Add helper for Gravater

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  # 与えられたユーザのGravater(http://gravater.com/)を返す
+  def gravater_for(user)
+    gravater_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravater_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
